06231bac718ecf9b971eda60c351b80ad9de824b
himanshu dhoundiyal <105155811+Geralt10@users.noreply.github.com>
2026-04-02 22:35:36 +0530

    CRUD

--- day-7/src/app.js
+++ day-7/src/app.js
@@ -18,5 +18,14 @@ app.post("/notes",async(req,res)=>{
 
 })
 
+app.get("/notes",async(req,res)=>{
+    const notes = await noteModel.find();
+
+    res.status(201).json({
+        message:"data fetch successfully",
+        notes
+    })
+})
+
 
 module.exports=app
