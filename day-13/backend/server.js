18780932451bba854853a466115c2f3039eab95e
himanshu dhoundiyal <105155811+Geralt10@users.noreply.github.com>
2026-06-20 14:03:44 +0530

    rm day-13

--- day-13/backend/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-const app = require("./src/app");
-const connectDB =require("./src/config/database");
-
-connectDB();
-app.listen(3000,()=>{
-    console.log("server is running at port 3000");
-    
-})
